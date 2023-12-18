0540471bcf3b48f1e9a7987f2807a66c370a4e3b
Ragai Ahmed <ragaiawad@gmail.com>
2023-12-19 00:07:25 +0200

    recheck config according to latest changes

--- control/low-level/movement/Config.h
+++ control/low-level/movement/Config.h
@@ -23,34 +23,35 @@
 
 
 // wheel pins 
-#define MOTOR_FR_EN  PA3
-#define MOTOR_FR_IN1 PA5
-#define MOTOR_FR_IN2 PA4
 
 #define MOTOR_FL_EN  PA1
 #define MOTOR_FL_IN1 PA2
 #define MOTOR_FL_IN2 PA0
 
+#define MOTOR_FR_EN  PA3
+#define MOTOR_FR_IN1 PA5
+#define MOTOR_FR_IN2 PA4
+
 #define MOTOR_BL_EN  PA6
 #define MOTOR_BL_IN1 PA7
 #define MOTOR_BL_IN2 PB0
 
-#define MOTOR_BR_EN  PB11
-#define MOTOR_BR_IN1 PB10
-#define MOTOR_BR_IN2 PB1
+#define MOTOR_BR_EN  PB1
+#define MOTOR_BR_IN1 PB11
+#define MOTOR_BR_IN2 PB10
 
 
-#define ENCODER_FR_A PB4   // motor0 
-#define ENCODER_FR_B PB3
 
-#define ENCODER_FL_A PA8   // motor1
-#define ENCODER_FL_B PA9 
+#define ENCODER_FL_A PB3   // motor0
+#define ENCODER_FL_B PB4 
 
+#define ENCODER_FR_A PA9   // motor1 
+#define ENCODER_FR_B PA8
 
-#define ENCODER_BR_A PB15   // motor2
-#define ENCODER_BR_B PB14
+#define ENCODER_BL_A PB14   // motor2
+#define ENCODER_BL_B PB15 
 
-#define ENCODER_BL_A PB13   // motor3
-#define ENCODER_BL_B PB14 
+#define ENCODER_BR_A PB13   // motor3
+#define ENCODER_BR_B PB12
 
 #endif
