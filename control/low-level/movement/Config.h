d2e99083d151b626f7f776511de61b68d15a404d
Zyad <zyadalaa606@gmail.com>
2023-12-18 20:05:55 +0200

    four wheels configured

--- control/low-level/movement/Config.h
+++ control/low-level/movement/Config.h
@@ -23,34 +23,34 @@
 
 
 // wheel pins 
-#define MOTOR0_EN  PA3
-#define MOTOR0_IN1 PA5
-#define MOTOR0_IN2 PA4
+#define MOTOR_FR_EN  PA3
+#define MOTOR_FR_IN1 PA5
+#define MOTOR_FR_IN2 PA4
 
-#define MOTOR1_EN  PA1
-#define MOTOR1_IN1 PA2
-#define MOTOR1_IN2 PA0
+#define MOTOR_FL_EN  PA1
+#define MOTOR_FL_IN1 PA2
+#define MOTOR_FL_IN2 PA0
 
-#define MOTOR2_EN  PA6
-#define MOTOR2_IN1 PA7
-#define MOTOR2_IN2 PB0
+#define MOTOR_BL_EN  PA6
+#define MOTOR_BL_IN1 PA7
+#define MOTOR_BL_IN2 PB0
 
-#define MOTOR3_EN  PB11
-#define MOTOR3_IN1 PB10
-#define MOTOR3_IN2 PB1
-
-
-#define ENCODER0_A PB4   // motor0 
-#define ENCODER0_B PB3
-
-#define ENCODER1_A PA8   // motor1
-#define ENCODER1_B PA9 
+#define MOTOR_BR_EN  PB11
+#define MOTOR_BR_IN1 PB10
+#define MOTOR_BR_IN2 PB1
 
 
-#define ENCODER2_A PB15   // motor2
-#define ENCODER2_B PB14
+#define ENCODER_FR_A PB4   // motor0 
+#define ENCODER_FR_B PB3
 
-#define ENCODER3_A PB13   // motor3
-#define ENCODER3_B PB14 
+#define ENCODER_FL_A PA8   // motor1
+#define ENCODER_FL_B PA9 
+
+
+#define ENCODER_BR_A PB15   // motor2
+#define ENCODER_BR_B PB14
+
+#define ENCODER_BL_A PB13   // motor3
+#define ENCODER_BL_B PB14 
 
 #endif
--- control/low-level/movement/movement.ino
+++ control/low-level/movement/movement.ino
@@ -4,7 +4,7 @@
 #include <std_msgs/Float32MultiArray.h>
 #include <std_msgs/Int32MultiArray.h>
 
-#include <Timer2.h>
+//#include <Timer2.h>
 #include "Encoder.h"
 #include "Config.h"
 #include "ISR_Encoder.h"
@@ -13,12 +13,15 @@
 void speedControl();
 
 //Encoder Objects
-ISR_Encoder MOTOR_Encoder[] {ISR_Encoder(RESOLUTION,ENCODER0_A, ENCODER0_B),
-                             ISR_Encoder(RESOLUTION,ENCODER1_A, ENCODER1_B),};
+ISR_Encoder MOTOR_Encoder[] {ISR_Encoder(RESOLUTION,ENCODER_FL_A, ENCODER_FL_B),
+                             ISR_Encoder(RESOLUTION,ENCODER_FR_A, ENCODER_FR_B),
+                             ISR_Encoder(RESOLUTION,ENCODER_BL_A, ENCODER_BL_B),
+                             ISR_Encoder(RESOLUTION,ENCODER_BR_A, ENCODER_BR_B)
+                             };
 
-long wheel_speeds [] = {0, 0};
+long wheel_speeds [] = {0, 0, 0, 0};
 
-Timer2 timer;
+//Timer2 timer;
 std_msgs::Float32MultiArray Espeed_msg;
 std_msgs::Int32MultiArray counts_msg;
 
@@ -26,7 +29,8 @@ void callback_speeds(const std_msgs::Float32MultiArray &speeds_msg)
 {
   wheel_speeds[0] = speeds_msg.data[0];
   wheel_speeds[1] = speeds_msg.data[1];
-
+  wheel_speeds[2] = speeds_msg.data[2];
+  wheel_speeds[3] = speeds_msg.data[3];
 }
 
 ros::NodeHandle nh;  // Initalizing the ROS node
@@ -42,8 +46,8 @@ wheel_vel_sub("wheel_vel",&callback_speeds);
 
 
 //Initializing some arrays
-long encoder_counts[] = {0, 0};
-float encoder_feedback[] = {0, 0};
+long encoder_counts[] = {0, 0, 0, 0};
+float encoder_feedback[] = {0, 0, 0, 0};
 
 
 
@@ -53,21 +57,28 @@ void setup()
   //Encoders Setup
   MOTOR_Encoder[0].setup();
   MOTOR_Encoder[1].setup();
-
+  MOTOR_Encoder[2].setup();
+  MOTOR_Encoder[3].setup();
+  
   //Seting the modes of the speed pins  
-  pinMode(MOTOR0_EN, OUTPUT);
-  pinMode(MOTOR1_EN, OUTPUT);
-
+  pinMode(MOTOR_FL_EN, OUTPUT);
+  pinMode(MOTOR_FR_EN, OUTPUT);
+  pinMode(MOTOR_BL_EN, OUTPUT);
+  pinMode(MOTOR_BR_EN, OUTPUT);
 
   //Setting the modes of the direction pins
-  pinMode(MOTOR0_IN1, OUTPUT);
-  pinMode(MOTOR0_IN2, OUTPUT);
-  pinMode(MOTOR1_IN1, OUTPUT);
-  pinMode(MOTOR1_IN2, OUTPUT);
-
+  pinMode(MOTOR_FL_IN1, OUTPUT);
+  pinMode(MOTOR_FL_IN2, OUTPUT);
+  pinMode(MOTOR_FR_IN1, OUTPUT);
+  pinMode(MOTOR_FR_IN2, OUTPUT);
+  pinMode(MOTOR_BL_IN1, OUTPUT);
+  pinMode(MOTOR_BL_IN2, OUTPUT);
+  pinMode(MOTOR_BR_IN1, OUTPUT);
+  pinMode(MOTOR_BR_IN2, OUTPUT);
+  
   analogWriteResolution(16);
 
-  timer.every(TIME_FREQ, TimerFunction);
+//  timer.every(TIME_FREQ, TimerFunction);
 
   //ROS node setup
   nh.initNode();
@@ -78,23 +89,35 @@ void setup()
 
 
   //*********************** Attaching intterupts to the encoder pins ********************************//
-  attachInterrupt(digitalPinToInterrupt(MOTOR_Encoder[0].PinA), Motor0_ISR_EncoderA, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(MOTOR_Encoder[0].PinB), Motor0_ISR_EncoderB, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(MOTOR_Encoder[0].PinA), Motor_FL_ISR_EncoderA, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(MOTOR_Encoder[0].PinB), Motor_FL_ISR_EncoderB, CHANGE);
 
-  attachInterrupt(digitalPinToInterrupt(MOTOR_Encoder[1].PinA), Motor1_ISR_EncoderA, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(MOTOR_Encoder[1].PinB), Motor1_ISR_EncoderB, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(MOTOR_Encoder[1].PinA), Motor_FR_ISR_EncoderA, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(MOTOR_Encoder[1].PinB), Motor_FR_ISR_EncoderB, CHANGE);
+
+  attachInterrupt(digitalPinToInterrupt(MOTOR_Encoder[2].PinA), Motor_BL_ISR_EncoderA, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(MOTOR_Encoder[2].PinB), Motor_BL_ISR_EncoderB, CHANGE);
+
+  attachInterrupt(digitalPinToInterrupt(MOTOR_Encoder[3].PinA), Motor_BR_ISR_EncoderA, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(MOTOR_Encoder[3].PinB), Motor_BR_ISR_EncoderB, CHANGE);
+
 
 
 
 }
 
 
-
+long long lastTime=0;
 void loop() 
 {
   nh.spinOnce();
 
-  timer.update();
+//  timer.update();
+  if(millis()-lastTime>= 1000/TIME_FREQ)
+  {
+     TimerFunction();
+     lastTime = millis();
+  }
 
 }
 
@@ -104,14 +127,18 @@ void TimerFunction()
 
   encoder_feedback[0] = { MOTOR_Encoder[0].calcspeed()};  
   encoder_feedback[1] = { MOTOR_Encoder[1].calcspeed()};
+  encoder_feedback[2] = { MOTOR_Encoder[2].calcspeed()};
+  encoder_feedback[3] = { MOTOR_Encoder[3].calcspeed()};
 
   encoder_counts[0] = { MOTOR_Encoder[0].counts};  
   encoder_counts[1] = { MOTOR_Encoder[1].counts};
+  encoder_counts[2] = { MOTOR_Encoder[2].counts};
+  encoder_counts[3] = { MOTOR_Encoder[3].counts};
 
-  Espeed_msg.data_length= 2;
+  Espeed_msg.data_length= 4;
   Espeed_msg.data= encoder_feedback;
 
-  counts_msg.data_length= 2;
+  counts_msg.data_length= 4;
   counts_msg.data= encoder_counts;
 
   speedControl();
@@ -123,52 +150,98 @@ void TimerFunction()
 
 void speedControl()
 {
-  // Drive Motor0
+  // Drive Motor FL
   if (wheel_speeds[0] > 0)
   {
-    digitalWrite(MOTOR0_IN1, HIGH);
-    digitalWrite(MOTOR0_IN2, LOW);
+    digitalWrite(MOTOR_FL_IN1, HIGH);
+    digitalWrite(MOTOR_FL_IN2, LOW);
   }
   else
   {
-    digitalWrite(MOTOR0_IN1, LOW);
-    digitalWrite(MOTOR0_IN2, HIGH);
+    digitalWrite(MOTOR_FL_IN1, LOW);
+    digitalWrite(MOTOR_FL_IN2, HIGH);
   }
-  analogWrite(MOTOR0_EN, constrain(abs(wheel_speeds[0]), MIN_VEL, MAX_VEL));
+  analogWrite(MOTOR_FL_EN, constrain(abs(wheel_speeds[0]), MIN_VEL, MAX_VEL));
 
 
-  // Drive Motor1
+  // Drive Motor FR
   if (wheel_speeds[1] > 0)
   {
-    digitalWrite(MOTOR1_IN1, HIGH);
-    digitalWrite(MOTOR1_IN2, LOW);
+    digitalWrite(MOTOR_FR_IN1, HIGH);
+    digitalWrite(MOTOR_FR_IN2, LOW);
   }
   else
   {
-    digitalWrite(MOTOR1_IN1, LOW);
-    digitalWrite(MOTOR1_IN2, HIGH);
+    digitalWrite(MOTOR_FR_IN1, LOW);
+    digitalWrite(MOTOR_FR_IN2, HIGH);
   }
-    analogWrite(MOTOR1_EN, constrain(abs(wheel_speeds[1]), MIN_VEL, MAX_VEL));
+  analogWrite(MOTOR_FR_EN, constrain(abs(wheel_speeds[1]), MIN_VEL, MAX_VEL));
 
-  
+    // Drive Motor BL
+  if (wheel_speeds[2] > 0)
+  {
+    digitalWrite(MOTOR_BL_IN1, HIGH);
+    digitalWrite(MOTOR_BL_IN2, LOW);
+  }
+  else
+  {
+    digitalWrite(MOTOR_BL_IN1, LOW);
+    digitalWrite(MOTOR_BL_IN2, HIGH);
+  }
+  analogWrite(MOTOR_BL_EN, constrain(abs(wheel_speeds[2]), MIN_VEL, MAX_VEL));
+
+
+  // Drive Motor BR
+  if (wheel_speeds[3] > 0)
+  {
+    digitalWrite(MOTOR_BR_IN1, HIGH);
+    digitalWrite(MOTOR_BR_IN2, LOW);
+  }
+  else
+  {
+    digitalWrite(MOTOR_BR_IN1, LOW);
+    digitalWrite(MOTOR_BR_IN2, HIGH);
+  }
+  analogWrite(MOTOR_BR_EN, constrain(abs(wheel_speeds[3]), MIN_VEL, MAX_VEL));
+
 }
 
-void Motor0_ISR_EncoderA()
+void Motor_FL_ISR_EncoderA()
 {
   MOTOR_Encoder[0].update_ISR_A();
 }
 
-void Motor0_ISR_EncoderB()
+void Motor_FL_ISR_EncoderB()
 {
   MOTOR_Encoder[0].update_ISR_B();
 }
 
-void Motor1_ISR_EncoderA()
+void Motor_FR_ISR_EncoderA()
 {
   MOTOR_Encoder[1].update_ISR_A();
 }
 
-void Motor1_ISR_EncoderB()
+void Motor_FR_ISR_EncoderB()
 {
   MOTOR_Encoder[1].update_ISR_B();
+}
+
+void Motor_BL_ISR_EncoderA()
+{
+  MOTOR_Encoder[2].update_ISR_A();
+}
+
+void Motor_BL_ISR_EncoderB()
+{
+  MOTOR_Encoder[2].update_ISR_B();
+}
+
+void Motor_BR_ISR_EncoderA()
+{
+  MOTOR_Encoder[3].update_ISR_A();
+}
+
+void Motor_BR_ISR_EncoderB()
+{
+  MOTOR_Encoder[3].update_ISR_B();
 }
