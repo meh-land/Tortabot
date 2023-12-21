a706cbf8d7b520deaf51307a16011fc11e96a948
YoussefAbbas2001 <youssefabbas.2001.1911@gmail.com>
2023-12-21 11:24:26 +0200

    Return tuned version of control

--- control/low-level/movement/Config.h
+++ control/low-level/movement/Config.h
@@ -2,59 +2,52 @@
 #define PINCONFIG_H
 
 
-#define DEAD_ZONE 2
+#define DEAD_ZONE 10
 #define numberOfVelocities 4
 #define sampleTime 0.03 //millisecond
-#define TIME_FREQ 30
-#define RESOLUTION 560
+#define TIME_FREQ 50
+#define RESOLUTION 2050
 #define minOut -65535
 #define maxOut 65535
 
-#define Kp0 800
-#define Ki0 0
-#define Kd0 0
+#define MAX_VEL 65535
+#define MIN_VEL 0
+#define kp_init 30000
+#define ki_init 30000
+#define kd_init 10
 
-#define Kp1 800
-#define Ki1 0  
-#define Kd1 0
-
-#define Kp2 800
-#define Ki2 0 
-#define Kd2 0
-
-#define Kp3 800
-#define Ki3 0
-#define Kd3 0
 
 
 // wheel pins 
-#define MOTOR0_EN  PB1
-#define MOTOR0_IN1 PB11
-#define MOTOR0_IN2 PB10
 
-#define MOTOR1_EN PA6
-#define MOTOR1_IN1 PA7
-#define MOTOR1_IN2 PB0
+#define MOTOR_FL_EN  PA1
+#define MOTOR_FL_IN1 PA2
+#define MOTOR_FL_IN2 PA0
 
-#define MOTOR2_EN PA3
-#define MOTOR2_IN1 PA5
-#define MOTOR2_IN2 PA4
+#define MOTOR_FR_EN  PA3
+#define MOTOR_FR_IN1 PA5
+#define MOTOR_FR_IN2 PA4
 
-#define MOTOR3_EN PA1
-#define MOTOR3_IN1 PA2
-#define MOTOR3_IN2 PA0
- 
-//Encoder Pins
-#define EnPin0 PB13  // motor0 
-#define EnPin1 PB12  
+#define MOTOR_BL_EN  PA6
+#define MOTOR_BL_IN1 PA7
+#define MOTOR_BL_IN2 PB0
 
-#define EnPin2 PB14   // motor1
-#define EnPin3 PB15  
+#define MOTOR_BR_EN  PB1
+#define MOTOR_BR_IN1 PB11
+#define MOTOR_BR_IN2 PB10
 
-#define EnPin4 PA8   // motor2
-#define EnPin5 PA9  
 
-#define EnPin6 PB4   // motor3  
-#define EnPin7 PB3
+
+#define ENCODER_FL_A PB3   // motor0
+#define ENCODER_FL_B PB4 
+
+#define ENCODER_FR_A PA9   // motor1 
+#define ENCODER_FR_B PA8
+
+#define ENCODER_BL_A PB14   // motor2
+#define ENCODER_BL_B PB15 
+
+#define ENCODER_BR_A PB13   // motor3
+#define ENCODER_BR_B PB12
 
 #endif
